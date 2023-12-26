9fdd2d8ce29505fccccab70ca95f43334968d802
Sunghyun Kim <ingnam920@gmail.com>
2023-12-26 22:11:23 +0900

    ã더하기 기능구현

---
+++ src/test/java/com/ll/CalcTest.java
@@ -0,0 +1,27 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CalcTest {
+    @Test
+    @DisplayName("1+1==2")
+    void t1(){
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+    }
+    @Test
+    @DisplayName("1+4==5")
+    void t2(){
+        assertThat(Calc.run("1 + 4")).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("50 - 30 == 20")
+    void t3(){
+        assertThat(Calc.run("50 - 30")).isEqualTo(20);
+    }
+
+}
